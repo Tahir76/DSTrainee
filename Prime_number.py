caddd33fd38348ccb68256a1efd2698c4e659cd9
Tahir76 <tahirrazakhan3099@gmail.com>
2023-02-16 15:26:05 +0500

    littlebitchange

--- Prime_number.py
+++ Prime_number.py
@@ -2,11 +2,11 @@ def is_prime(n):
     sum=0
     if n <= 1:
         return -1
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, n):
         if n % i == 0:
             return -1
     for i in range(0,n+1):
         sum =i+sum
     return sum
 
-print(is_prime(10))
+print(is_prime(7))
